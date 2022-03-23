112eaeaa26f544a1b7294f9a233b2dee858c714c
xalivan <xalivan@ukr.net>
2022-03-23 15:27:26 +0200

    1.Speaker. Rest controller

---
+++ src/main/java/com/speaker/SpeakerApplication.java
@@ -0,0 +1,13 @@
+package com.speaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/speaker/SpeakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
