92109791cf06c555a82116503455cef7570687da
xalivan <xalivan@ukr.net>
2022-03-30 10:19:45 +0300

    4.1. Swagger

--- src/main/java/com/speaker/entities/Account.java
+++ src/main/java/com/speaker/entities/Account.java
@@ -1,10 +1,15 @@
 package com.speaker.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
 import lombok.Builder;
 import lombok.Data;
+
 @Data
 @Builder
+@ApiModel
 public class Account {
+    @JsonIgnore
     private final int id;
     private final String name;
     private final String lastName;
