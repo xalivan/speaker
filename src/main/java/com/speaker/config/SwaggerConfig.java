b0225da9b200062cb393078316315e4b90e63765
xalivan <xalivan@ukr.net>
2022-03-30 12:01:36 +0300

    4.3. Swagger route

--- src/main/java/com/speaker/config/SwaggerConfig.java
+++ src/main/java/com/speaker/config/SwaggerConfig.java
@@ -2,10 +2,17 @@ package com.speaker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +23,11 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.speaker.controller"))
                 .paths(PathSelectors.ant("/accounts"))
                 .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction() {
+        return route(GET("/swagger"), req ->
+                ServerResponse.temporaryRedirect(URI.create("swagger-ui.html")).build());
     }
 }
 
