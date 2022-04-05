dee88f01a972bc4984616a651fb4c2042d235b68
xalivan <xalivan@ukr.net>
2022-04-05 17:41:27 +0300

    9.1.Find all Accounts with friends

--- src/main/java/com/speaker/dto/AccountDTO.java
+++ src/main/java/com/speaker/dto/AccountDTO.java
@@ -1,6 +1,5 @@
 package com.speaker.dto;
 
-import com.speaker.entities.Account;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,12 +8,11 @@ import java.util.List;
 @Data
 @Builder
 public class AccountDTO {
-
     private final int id;
     private final String name;
     private final String lastName;
     private final int age;
     private final CountryDTO country;
-    private final List<Account> friends;
+    private final List<AccountDTO> friends;
     private final List<MessageDTO> massages;
 }
