d7695c7f847f66f0706bc080d7c71ef611a33e3f
xalivan <xalivan@ukr.net>
2022-04-08 10:56:33 +0300

    10.6.Get all country and city, filter from UI and getIdes  @PostConstruct

--- src/main/java/com/speaker/service/AccountServiceImpl.java
+++ src/main/java/com/speaker/service/AccountServiceImpl.java
@@ -5,7 +5,6 @@ import com.speaker.dto.AccountDTO;
 import com.speaker.entities.*;
 import com.speaker.repository.AccountRepository;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,10 +12,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 @Service
-@RequiredArgsConstructor
+@Data
 public class AccountServiceImpl implements AccountService {
+    private Map<CountryName, Map<CityName, Pair<Integer, Integer>>> mapCountryAndCity;
     private final AccountRepository accountRepository;
     private final AccountConverter accountConverter;
 
@@ -34,20 +33,21 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public boolean create(AccountDTO accountDTO) {
-        Map<CityName, Pair<Integer, Integer>> cityNamePairMap = getAllCountryAndCity().get(accountDTO.getCountry().getName());
+        Map<CityName, Pair<Integer, Integer>> cityNamePairMap = mapCountryAndCity.get(accountDTO.getCountry().getName());
         Pair<Integer, Integer> integerIntegerPair = cityNamePairMap.get(accountDTO.getCountry().getCityDTO().getName());
         return accountRepository.insert(accountConverter
                 .convertToAccount(accountDTO, integerIntegerPair.getFirst(), integerIntegerPair.getSecond())) > 0;
     }
 
     @PostConstruct
-    private Map<CountryName, Map<CityName, Pair<Integer, Integer>>> getAllCountryAndCity() {
+    private void getAllCountryAndCity() {
         List<Country> allCountryAndCity = accountRepository.findAllCountryAndCity();
-        return allCountryAndCity.stream()
+        mapCountryAndCity = allCountryAndCity.stream()
                 .map(country -> new Pair<CountryName, Pair<CityName, Pair<Integer, City>>>(country.getName(),
                         new Pair<CityName, Pair<Integer, City>>(country.getCity().getName(),
                                 new Pair<Integer, City>(country.getId(), country.getCity()))
-                )).collect(Collectors.groupingBy(Pair::getFirst, Collectors.mapping(Pair::getSecond, Collectors.toMap(Pair::getFirst, this::convertToIds, (city1, city2) -> city1))));
+                )).collect(Collectors.groupingBy(Pair::getFirst, Collectors.mapping(Pair::getSecond,
+                        Collectors.toMap(Pair::getFirst, this::convertToIds, (city1, city2) -> city1))));
     }
 
     private Pair<Integer, Integer> convertToIds(Pair<CityName, Pair<Integer, City>> entry) {
